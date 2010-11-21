22df7d990b1b7141fd30e2ce861ba5a0d90de3a2
Kim Silkebækken <kim.silkebaekken@gmail.com>
2010-11-21 17:36:45 +0100

    dwm: Add comboview/combotag patch

--- pkg/dwm-hg/config.h
+++ pkg/dwm-hg/config.h
@@ -80,9 +80,9 @@ dwm_button(const Arg *arg)
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY,                       KEY,      comboview,      {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      combotag,       {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */

e08ec96c9265dada921ef0a21de787bada7b9b6d
Kim Silkebækken <kim.silkebaekken@gmail.com>
2011-04-26 22:02:58 +0200

    dwm: Update configuration

--- pkg/dwm-git/config.h
+++ pkg/dwm-git/config.h
@@ -1,3 +1,6 @@
+// Makes XF86* keys work
+#include <X11/XF86keysym.h>
+
 // Appearance
 #define NUMCOLORS 8
 static const char colors[NUMCOLORS][ColLast][8] = {
@@ -12,10 +15,10 @@ static const char colors[NUMCOLORS][ColLast][8] = {
 	{ "#000000", "#daff30", "#1a1a1a" },  //
 };
 static const char font[]            = "-artwiz-cureextra-medium-*-*-*-*-*-*-*-*-*-*-*";
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const Bool showbar           = True;     /* False means no bar */
-static const Bool topbar            = True;     /* False means bottom bar */
+static const unsigned int borderpx  = 1;        // border pixel of windows
+static const unsigned int snap      = 32;       // snap pixel
+static const Bool showbar           = True;     // False means no bar
+static const Bool topbar            = True;     // False means bottom bar
 
 // Layout(s)
 static const float mfact      = 0.68;   // factor of master area size [0.05..0.95]
@@ -35,8 +38,8 @@ static const Tag tags[] = {
 	// name       layout           mfact
 	{ "Ä Main",     &layouts[0],     -1   },
 	{ "Ã Web",      &layouts[0],     0.5  },
-	{ "Â Dev",      &layouts[4],     0.7  },
-	{ "Ç Misc",     &layouts[3],     -1   },
+	{ "Â Dev",      &layouts[0],     0.8  },
+	{ "Ç Media",    &layouts[0],     0.8  },
 	{ "Á IM",       &layouts[2],     -1   },
 };
 
@@ -48,9 +51,8 @@ static const Rule rules[] = {
 	{ "MPlayer",  NULL,       NULL,       0,            True,        -1 },
 	{ "Chromium", NULL,       NULL,       1<<1,         False,       -1 },
 	{ "URxvt",    "vim",      NULL,       1<<2,         False,       -1 },
-	{ "URxvt",    "ncmpcpp",  NULL,       0,            False,       -1 },
+	{ "URxvt",    "ncmpcpp",  NULL,       1<<3,         False,       -1 },
 	{ "URxvt",    "ranger",   NULL,       0,            False,       -1 },
-	{ "URxvt",    "mutt",     NULL,       1<<3,         False,       -1 },
 	{ "URxvt",    "weechat",  NULL,       1<<4,         False,       -1 },
 };
 
@@ -73,67 +75,75 @@ restart(const Arg *arg)
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
-static const char terminal[]  = "urxvtc";
+static const char terminal[] = "urxvtc";
 
 // Commands
-static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
-static const char *chromium[] = { "/home/kim/sync/bin/chromium", NULL };
-static const char *termcmd[]  = { terminal, NULL };
-static const char *vim[]      = { terminal, "-name", "vim",     "-e", "vim",            NULL };
-static const char *ncmpcpp[]  = { terminal, "-name", "ncmpcpp", "-e", "ncmpcpp",        NULL };
-static const char *ranger[]   = { terminal, "-name", "ranger",  "-e", "ranger",         NULL };
-static const char *mutt[]     = { terminal, "-name", "mutt",    "-e", "mutt",           NULL };
-static const char *weechat[]  = { terminal, "-name", "weechat", "-e", "weechat-curses", NULL };
-static const char *scrot[]    = { "scrot", NULL };
+static const char *term_cmd[]                 = { terminal, NULL };
+static const char *dmenu_cmd[]                = { "dmenu_run", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
+static const char *screenshot_cmd[]           = { "scrot", NULL };
+
+static const char *run_chromium[]             = { "/home/kim/sync/bin/chromium",     NULL };
+static const char *run_chromium_tor[]         = { "/home/kim/sync/bin/chromium-tor", NULL };
+
+static const char *run_vim[]                  = { terminal, "-name", "vim",        "-e", "vim",                NULL };
+static const char *run_ncmpcpp[]              = { terminal, "-name", "ncmpcpp",    "-e", "ncmpcpp",            NULL };
+static const char *run_ranger[]               = { terminal, "-name", "ranger",     "-e", "ranger",             NULL };
+static const char *run_weechat[]              = { terminal, "-name", "weechat",    "-e", "weechat-curses",     NULL };
+
+static const char *control_volume_lower[]     = { "amixer", "set", "Master", "10-%", NULL };
+static const char *control_volume_raise[]     = { "amixer", "set", "Master", "10+%", NULL };
+
+static const char *control_shutdown[]         = { "sudo", "shutdown", "-h", "now", NULL };
+static const char *control_reboot[]           = { "sudo", "shutdown", "-r", "now", NULL };
 
 static Key keys[] = {
-	// modifier                     key        function        argument
-	{ MODKEY,                       XK_x,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_n,      spawn,          {.v = chromium } },
-	{ MODKEY,                       XK_v,      spawn,          {.v = vim } },
-	{ MODKEY|ShiftMask,             XK_n,      spawn,          {.v = ncmpcpp } },
-	{ MODKEY,                       XK_r,      spawn,          {.v = ranger } },
-	{ MODKEY|ShiftMask,             XK_m,      spawn,          {.v = mutt } },
-	{ MODKEY,                       XK_c,      spawn,          {.v = weechat } },
-	{ 0,                            XK_Print,  spawn,          {.v = scrot } },
+	// modifier               key           function          argument
+	{ MODKEY,                 XK_x,         spawn,            {.v = dmenu_cmd } },
+	{ MODKEY,                 XK_Return,    spawn,            {.v = term_cmd } },
+	{ MODKEY,                 XK_m,         spawn,            {.v = run_ncmpcpp } },
+	{ MODKEY,                 XK_n,         spawn,            {.v = run_chromium } },
+	{ MODKEY|ShiftMask,       XK_n,         spawn,            {.v = run_chromium_tor } },
+	{ MODKEY,                 XK_v,         spawn,            {.v = run_vim } },
+	{ MODKEY,                 XK_r,         spawn,            {.v = run_ranger } },
+	{ MODKEY,                 XK_c,         spawn,            {.v = run_weechat } },
 
-	{ MODKEY,                       XK_Up,     focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_Down,   focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_Right,  setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Left,   setmfact,       {.f = -0.05} },
+	{ MODKEY,                 XK_h,         setmfact,         {.f = -0.05} },
+	{ MODKEY,                 XK_j,         focusstack,       {.i = -1 } },
+	{ MODKEY,                 XK_k,         focusstack,       {.i = +1 } },
+	{ MODKEY,                 XK_l,         setmfact,         {.f = +0.05} },
 
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                 XK_Tab,       view,             {0} },
+	{ MODKEY,                 XK_Escape,    killclient,       {0} },
+	{ MODKEY,                 XK_b,         togglebar,        {0} },
+	{ MODKEY,                 XK_space,     setlayout,        {0} },
+	{ MODKEY|ShiftMask,       XK_space,     togglefloating,   {0} },
+	{ MODKEY,                 XK_comma,     focusmon,         {.i = -1 } },
+	{ MODKEY,                 XK_period,    focusmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,       XK_comma,     tagmon,           {.i = -1 } },
+	{ MODKEY|ShiftMask,       XK_period,    tagmon,           {.i = +1 } },
 
-	{ Mod1Mask,                     XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_Escape, killclient,     {0} },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ 0,                      XK_Print,     spawn,            {.v = screenshot_cmd } },
+	{ Mod1Mask,               XK_Return,    zoom,             {0} },
 
-	TAGKEYS(                        XK_a,                      0)
-	TAGKEYS(                        XK_o,                      1)
-	TAGKEYS(                        XK_e,                      2)
-	TAGKEYS(                        XK_u,                      3)
-	TAGKEYS(                        XK_i,                      4)
+	TAGKEYS(                  XK_a,                           0)
+	TAGKEYS(                  XK_o,                           1)
+	TAGKEYS(                  XK_e,                           2)
+	TAGKEYS(                  XK_u,                           3)
+	TAGKEYS(                  XK_i,                           4)
 
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ControlMask,     XK_t,         setlayout,        {.v = &layouts[0]} }, // (t)ile
+	{ MODKEY|ControlMask,     XK_f,         setlayout,        {.v = &layouts[1]} }, // (f)loating
+	{ MODKEY|ControlMask,     XK_m,         setlayout,        {.v = &layouts[2]} }, // (m)onocle
+	{ MODKEY|ControlMask,     XK_g,         setlayout,        {.v = &layouts[3]} }, // (g)aplessgrid
+	{ MODKEY|ControlMask,     XK_b,         setlayout,        {.v = &layouts[4]} }, // (b)stack
 
-	{ MODKEY|ShiftMask,             XK_Escape, quit,           {0} },
-	{ MODKEY|ShiftMask,             XK_r,      restart,        {0} },
+	{ MODKEY|ShiftMask,       XK_Escape,    quit,             {0} },
+	{ MODKEY|ShiftMask,       XK_r,         restart,          {0} },
+	{ MODKEY|ControlMask,     XK_Escape,    spawn,            {.v = control_shutdown } },
+	{ MODKEY|ControlMask,     XK_r,         spawn,            {.v = control_reboot } },
+
+	{ 0,                      XF86XK_AudioLowerVolume,   spawn, {.v = control_volume_lower } },
+	{ 0,                      XF86XK_AudioRaiseVolume,   spawn, {.v = control_volume_raise } },
 };
 
 // Button definitions
@@ -143,7 +153,6 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },

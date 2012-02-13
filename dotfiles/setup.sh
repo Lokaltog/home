7133ab0f2b5087db46e0712f034a34ef9c49b474
Kim Silkebækken <kim.silkebaekken@gmail.com>
2012-02-13 09:10:43 +0100

    Reorganize dotfiles directory

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -3,7 +3,7 @@
 
 FROM="`dirname \`which $0\``"
 
-if [ -d "$1" ]; then
+if [[ -d "$1" ]]; then
 	TO="$1"
 else
 	TO="$PWD"
@@ -11,6 +11,25 @@ fi
 
 cd $FROM
 
-for FILE in dot.*; do
-	ln -sT "$FROM/$FILE" "$TO/${FILE:3}"
+for FILE in `find -L . -maxdepth 1 -wholename './.*' -printf '%P\n'`; do
+	TO_FILE="$TO/$FILE"
+
+	if [[ -e $TO_FILE ]] || [[ -h $TO_$FILE ]]; then
+		read -p "File $TO_FILE exists, overwrite? [Y/n] " -n 1 -r
+
+		if [[ $REPLY =~ ^[Nn]$ ]]; then
+			echo
+			echo "Skipping $FILE"
+			echo
+
+			continue
+		fi
+
+		rm $TO_FILE
+	fi
+
+	echo "Creating symlink to $FILE"
+	echo
+
+	ln -sT "$FROM/$FILE" $TO_FILE
 done

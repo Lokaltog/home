6da95fa3c6acc4fbf2f557dd9616b4594852701e
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-01-16 13:37:44 +0100

    Add virtualfish to config.fish

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -60,3 +60,7 @@ set -x PATH $PATH $HOME/.local/bin $HOME/.gem/ruby/1.9.1/bin $HOME/sync/bin /usr
 set -x NODE_PATH /usr/lib/node_modules $NODE_PATH
 
 eval (dircolors -c ~/sync/modules/dircolors/LS_COLORS)
+
+# virtualenv support
+. ~/sync/modules/virtualfish/virtual.fish
+. ~/sync/modules/virtualfish/auto_activation.fish

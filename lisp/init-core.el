5495e8ca93e23cbcb7ada0d81be8e53d8e4c7fc6
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-03-30 15:52:46 +0200

    Add some extra font lock keywords to Python/C code

--- lisp/init-core.el
+++ lisp/init-core.el
@@ -51,4 +51,74 @@
 
 (load custom-file)
 
+;; add extra font lock keywords
+(defun lt/kw-add-fixme ()
+  (font-lock-add-keywords nil
+                          '(("\\<\\(FIXME\\|TODO\\|BUG\\)\\>"
+                             1 font-lock-warning-face t))))
+
+(defun lt/kw-add-constant ()
+  (font-lock-add-keywords nil
+                          '(("[^\"]\\<\\([A-Z_][A-Z0-9_]+\\)\\>[^(\"]"
+                             1 font-lock-constant-face t))))
+
+(defvar font-lock-number-face 'font-lock-number-face
+  "Face name to use for numbers.")
+
+(defface font-lock-number-face
+  '((t (:foreground "orange")))
+  "Font Lock mode face used to highlight numbers."
+  :group 'font-lock-faces)
+
+(defun lt/kw-add-numbers ()
+    (font-lock-add-keywords
+     nil '(
+           ;; Valid hex number (will highlight invalid suffix though)
+           ("\\b0x[[:xdigit:]]+[uUlL]*\\b" . font-lock-number-face)
+           ;; Invalid hex number
+           ("\\b0x\\(\\w\\|\\.\\)+\\b" . font-lock-warning-face)
+           ;; Valid floating point number.
+           ("\\(\\b[0-9]+\\|\\)\\(\\.\\)\\([0-9]+\\(e[-]?[0-9]+\\)?\\([lL]?\\|[dD]?[fF]?\\)\\)\\b" (1 font-lock-number-face) (3 font-lock-number-face))
+           ;; Invalid floating point number.  Must be before valid decimal.
+           ("\\b[0-9].*?\\..+?\\b" . font-lock-warning-face)
+           ;; Valid decimal number.  Must be before octal regexes otherwise 0 and 0l
+           ;; will be highlighted as errors.  Will highlight invalid suffix though.
+           ("\\b\\(\\(0\\|[1-9][0-9]*\\)[uUlL]*\\)\\b" 1 font-lock-number-face)
+           ;; Valid octal number
+           ("\\b0[0-7]+[uUlL]*\\b" . font-lock-number-face)
+           ;; Floating point number with no digits after the period.  This must be
+           ;; after the invalid numbers, otherwise it will "steal" some invalid
+           ;; numbers and highlight them as valid.
+           ("\\b\\([0-9]+\\)\\." (1 font-lock-number-face))
+           ;; Invalid number.  Must be last so it only highlights anything not
+           ;; matched above.
+           ("\\b[0-9]\\(\\w\\|\\.\\)+?\\b" . font-lock-warning-face)
+           )))
+
+(defvar font-lock-pointer-face 'font-lock-pointer-face
+  "Face name to use for C pointers.")
+
+(defface font-lock-pointer-face
+  '((t (:foreground "orange")))
+  "Font Lock mode face used to highlight C pointers."
+  :group 'font-lock-faces)
+
+(defun lt/kw-add-pointers ()
+  (font-lock-add-keywords nil
+                          '(("\\(\\*+\\)[a-z]"
+                             1 font-lock-pointer-face t))))
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (lt/kw-add-numbers)
+            (lt/kw-add-fixme)
+            (lt/kw-add-constant)
+            (lt/kw-add-pointers)))
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (lt/kw-add-numbers)
+            (lt/kw-add-fixme)
+            (lt/kw-add-constant)))
+
 (provide 'init-core)

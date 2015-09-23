b43b0307554d3c63753dcfd84f56e9163841a9ae
Kim Silkebækken <kim@silkebaekken.no>
2015-09-23 22:51:16 +0200

    emacs: Update config

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -34,6 +34,7 @@ re-downloaded in order to locate PACKAGE."
 (setq-default
  inhibit-splash-screen t
  inhibit-startup-message t
+ initial-scratch-message nil
  backup-inhibited t
  auto-save-default nil
  create-lockfiles nil
@@ -55,7 +56,8 @@ re-downloaded in order to locate PACKAGE."
  gc-cons-threshold 20000000
  vc-follow-symlink t
  blink-cursor-alist '((box . hbar))
- custom-file (expand-file-name "init-custom.el" user-emacs-directory))
+ custom-file (expand-file-name "init-custom.el" user-emacs-directory)
+ echo-keystrokes 0.02)
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
@@ -78,9 +80,10 @@ re-downloaded in order to locate PACKAGE."
 (line-number-mode t)
 
 ;; Set encoding to always be UTF-8
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8-unix)
+(set-terminal-coding-system 'utf-8-unix)
+(set-keyboard-coding-system 'utf-8-unix)
+(prefer-coding-system 'utf-8-unix)
 
 ;; Make inactive frames transparent
 (set-frame-parameter (selected-frame) 'alpha '(100 80))
@@ -112,294 +115,334 @@ This functions should be added to the hooks of major modes for programming."
                 tab-width (default-value 'tab-width)
                 sws-tab-width (default-value 'tab-width)
                 sass-indent-offset (default-value 'tab-width)
-                jade-tab-width (default-value 'tab-width)
-                python-indent (default-value 'tab-width)))
+                jade-tab-width (default-value 'tab-width)))
 
-(dolist (hook '(json nginx vcl sws text jade sass python sql))
+(dolist (hook '(json nginx vcl sws text jade sass sql))
   (add-hook (intern (concat (symbol-name hook) "-mode-hook")) 'lokaltog-set-tabs-mode-hook))
 
 ;; Setup packages
+(use-package server
+  :if window-system
+  :config
+  (unless (server-running-p)
+    (server-start)))
+
 (use-package distinguished-theme
-             :ensure t
-             :config
-             (load-theme 'distinguished t))
+  :ensure t
+  :config
+  (load-theme 'distinguished t))
+
+(use-package popwin
+  :commands popwin-mode
+  :config
+  (popwin-mode 1)
+  (push '("*Compile-Log*" :height 20 :noselect t) popwin:special-display-config))
 
 (use-package rainbow-delimiters
-             :ensure t
-             :config
-             (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
 (use-package rainbow-mode
-             :ensure t
-             :config
-             (add-hook 'prog-mode-hook #'rainbow-mode))
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-mode))
 
 (use-package highlight-numbers
-             :ensure t
-             :config
-             (add-hook 'prog-mode-hook #'highlight-numbers-mode))
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'highlight-numbers-mode))
+
+(use-package smartparens-config
+  :ensure smartparens
+  :config
+  (show-smartparens-global-mode t)
+  (add-hook 'prog-mode-hook #'turn-on-smartparens-strict-mode))
 
 (use-package prog-mode
-             :defer t)
+  :defer t)
+
+(use-package guide-key
+  :commands guide-key-mode
+  :ensure t
+  :init
+  (setq guide-key/guide-key-sequence '(","       ; Evil leader
+                                       "g"
+                                       "z"
+                                       "C-h"     ; Help
+                                       "C-x r"   ; Register commands
+                                       "C-x 4"   ; Other window commands
+                                       "C-x 5"   ; Other frame commands
+                                       "C-x c"   ; Helm prefix
+                                       "C-c")    ; Mode commands
+        guide-key/idle-delay 0.1)
+  (guide-key-mode 1))
 
 (use-package evil
-             :ensure t
-             :init
-             (defun def-assoc (key alist default)
-               "Return cdr of `KEY' in `ALIST' or `DEFAULT' if key is no car in alist."
-               (let ((match (assoc key alist)))
-                 (if match
-                     (cdr match)
-                   default)))
-             (defun lokaltog-evil-cursor ()
-               "Change cursor color according to evil-state."
-               (let ((cursor-colors-default "#ffe326")
-                     (cursor-colors '((insert . "#a5e724")
-                                      (visual . "#b0b3ba")
-                                      (replace . "#ff4a52")))
-                     (cursor-types-default 'box)
-                     (cursor-types '((insert . 'bar)
-                                     (visual . 'box))))
-                 (setq cursor-type (def-assoc evil-state cursor-types cursor-types-default))
-                 (set-cursor-color (def-assoc evil-state cursor-colors cursor-colors-default))))
-             (setq evil-default-cursor #'lokaltog-evil-cursor)
+  :ensure t
+  :init
+  (defun def-assoc (key alist default)
+    "Return cdr of `KEY' in `ALIST' or `DEFAULT' if key is no car in alist."
+    (let ((match (assoc key alist)))
+      (if match
+          (cdr match)
+        default)))
+  (defun lokaltog-evil-cursor ()
+    "Change cursor color according to evil-state."
+    (let ((cursor-colors-default "#ffe326")
+          (cursor-colors '((insert . "#a5e724")
+                           (visual . "#b0b3ba")
+                           (replace . "#ff4a52")))
+          (cursor-types-default 'box)
+          (cursor-types '((insert . 'bar)
+                          (visual . 'box))))
+      (setq cursor-type (def-assoc evil-state cursor-types cursor-types-default))
+      (set-cursor-color (def-assoc evil-state cursor-colors cursor-colors-default))))
+  (setq evil-default-cursor #'lokaltog-evil-cursor)
+  :config
+  (define-key evil-normal-state-map (kbd "TAB") 'evil-indent-line)
+  (define-key evil-visual-state-map (kbd "TAB") 'indent-region)
 
-             (use-package evil-leader
-                          :ensure t
-                          :init
-                          (global-evil-leader-mode))
+  (define-key evil-motion-state-map ";" 'evil-ex)
+  (define-key evil-motion-state-map ":" 'evil-repeat-find-char)
+  (evil-mode 1))
 
-             :config
-             (define-key evil-normal-state-map (kbd "TAB") 'evil-indent-line)
-             (define-key evil-visual-state-map (kbd "TAB") 'indent-region)
-             (define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)
+(use-package evil-leader
+  :ensure t
+  :config
+  (global-evil-leader-mode)
+  (evil-leader/set-leader ",")
+  (evil-leader/set-key
+    "b" 'switch-to-buffer
+    "B" 'projectile-switch-to-buffer
+    "F" 'make-frame-command
+    "f" 'projectile-find-file
+    "t" 'projectile-find-tag
+    "kb" 'kill-this-buffer
+    "rt" 'projectile-regenerate-tags
+    "p" 'projectile-switch-project))
 
-             (define-key evil-motion-state-map ";" 'evil-ex)
-             (define-key evil-motion-state-map ":" 'evil-repeat-find-char)
+(use-package god-mode
+  :ensure t)
 
-             (evil-mode 1))
-
-(use-package smartparens
-             :ensure t)
+(use-package evil-god-state
+  :ensure t
+  :commands evil-execute-in-god-state
+  :init
+  (define-key evil-normal-state-map (kbd "SPC") 'evil-execute-in-god-state)
+  (evil-define-key 'god global-map [escape] 'evil-god-state-bail))
 
 (use-package undo-tree
-             :ensure t
-             :config
-             (global-undo-tree-mode))
+  :ensure t
+  :config
+  (global-undo-tree-mode))
 
 (use-package whitespace
-             :ensure t
-             :init
-             (setq whitespace-style (quote (face tabs newline tab-mark newline-mark))
-                   whitespace-display-mappings '((newline-mark ?\n [?· ?\n])
-                                                 (tab-mark     ?\t [?┊ ?\t] [?\\ ?\t])))
-             :config
-             (global-whitespace-mode t))
+  :ensure t
+  :init
+  (setq whitespace-style (quote (face tabs newline tab-mark newline-mark))
+        whitespace-display-mappings '((newline-mark ?\n [?· ?\n])
+                                      (tab-mark     ?\t [?┊ ?\t] [?\\ ?\t])))
+  :config
+  (global-whitespace-mode t))
 
 (use-package magit
-             :ensure t)
+  :ensure t)
 
 (use-package git-gutter-fringe
-             :ensure t
-             :init
-             (setq git-gutter-fr:side 'right-fringe)
-             :config
-             (global-git-gutter-mode t))
+  :ensure t
+  :init
+  (setq git-gutter-fr:side 'right-fringe)
+  :config
+  (global-git-gutter-mode t))
 
 (use-package flx-ido)
-             :ensure t
+:ensure t
 
 (use-package ido-vertical-mode)
-             :ensure t
+:ensure t
 
 (use-package smex
-             :ensure t
-             :config
-             (smex-initialize)
-             :bind
-             ("M-x" . smex))
+  :ensure t
+  :config
+  (smex-initialize)
+  :bind
+  ("M-x" . smex))
 
 (use-package ido
-             :ensure t
-             :init
-             (setq ido-enable-prefix nil
-                   ido-enable-flex-matching t
-                   ido-create-new-buffer 'always
-                   ido-use-filename-at-point 'guess
-                   ido-max-prospects 20
-                   ido-use-faces nil
-                   ido-default-file-method 'selected-window)
-             :config
-             (flx-ido-mode t)
-             (ido-mode 1)
-             (ido-everywhere 1)
-             (ido-vertical-mode))
+  :ensure t
+  :init
+  (setq ido-enable-prefix nil
+        ido-enable-flex-matching t
+        ido-create-new-buffer 'always
+        ido-use-filename-at-point 'guess
+        ido-max-prospects 20
+        ido-use-faces nil
+        ido-default-file-method 'selected-window)
+  :config
+  (flx-ido-mode t)
+  (ido-mode 1)
+  (ido-everywhere 1)
+  (ido-vertical-mode))
 
 (use-package pos-tip
-             :ensure t
-             :init
-             (setq x-gtk-use-system-tooltips nil))
+  :ensure t
+  :init
+  (setq x-gtk-use-system-tooltips nil))
 
 (use-package flycheck-pos-tip
-             :ensure t
-             :init
-             (eval-after-load 'flycheck
-               '(custom-set-variables
-                 '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages))))
+  :ensure t
+  :init
+  (eval-after-load 'flycheck
+    '(custom-set-variables
+      '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages))))
 
 (use-package company
-             :ensure t
-             :config
-             (global-company-mode)
+  :ensure t
+  :config
+  (global-company-mode)
 
-             ; Fix tabbing issues
-             (defun indent-or-complete ()
-               (interactive)
-               (if (looking-at "\\_>")
-                   (company-complete-common)
-                 (indent-according-to-mode)))
-             (define-key evil-insert-state-map (kbd "TAB") 'indent-or-complete)
+  ;; Fix tabbing issues
+  (defun indent-or-complete ()
+    (interactive)
+    (if (looking-at "\\_>")
+        (company-complete-common)
+      (indent-according-to-mode)))
+  (define-key evil-insert-state-map (kbd "TAB") 'indent-or-complete)
 
-             ; Fix evil repeat action issue
-             (mapc #'evil-declare-change-repeat
-                   '(company-complete-common
-                     company-select-next
-                     company-select-previous
-                     company-complete-selection
-                     ))
+  ;; Fix evil repeat action issue
+  (mapc #'evil-declare-change-repeat
+        '(company-complete-common
+          company-select-next
+          company-select-previous
+          company-complete-selection
+          ))
 
-             ; Navigate menu with C-n, C-p and tab
-             (with-eval-after-load 'company
-               (define-key company-active-map (kbd "M-n") nil)
-               (define-key company-active-map (kbd "M-p") nil)
-               (define-key company-active-map (kbd "C-n") #'company-select-next)
-               (define-key company-active-map (kbd "C-p") #'company-select-previous)
+  ;; Navigate menu with C-n, C-p and tab
+  (with-eval-after-load 'company
+    (define-key company-active-map (kbd "M-n") nil)
+    (define-key company-active-map (kbd "M-p") nil)
+    (define-key company-active-map (kbd "C-n") #'company-select-next)
+    (define-key company-active-map (kbd "C-p") #'company-select-previous)
 
-               (define-key company-active-map [tab] 'company-complete-common-or-cycle)
-               (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)))
+    (define-key company-active-map [tab] 'company-complete-common-or-cycle)
+    (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)))
 
 (use-package paren
-             :init
-             (setq show-paren-delay 0)
-             :config
-             (show-paren-mode t))
+  :init
+  (setq show-paren-delay 0)
+  :config
+  (show-paren-mode t))
 
 (use-package projectile
-             :ensure t
-             :config
-             (projectile-global-mode)
-             (setq projectile-globally-ignored-files (append projectile-globally-ignored-files '("waf" "'.lock-waf*'"))
-                   projectile-globally-ignored-directories (append projectile-globally-ignored-directories '("'.waf*'" "build" "out" "venv")))
-             :bind
-             ("<f5>" . make-frame-command)
-             ("<C-f9>" . projectile-switch-to-buffer)
-             ("<f9>" . switch-to-buffer)
-             ("<f10>" . projectile-find-file)
-             ("<f11>" . projectile-find-tag)
-             ("<C-f11>" . projectile-regenerate-tags)
-             ("<f12>" . projectile-switch-project))
-
-(use-package project-explorer
-             :ensure t)
+  :ensure t
+  :config
+  (projectile-global-mode)
+  (setq projectile-globally-ignored-files (append projectile-globally-ignored-files '("waf" "'.lock-waf*'"))
+        projectile-globally-ignored-directories (append projectile-globally-ignored-directories '("'.waf*'" "build" "out" "venv"))))
 
 (use-package smart-mode-line
-             :ensure t
-             :config
-             (add-to-list 'sml/replacer-regexp-list '("^~/projects" ":proj:"))
-             (sml/setup))
+  :ensure t
+  :config
+  (add-to-list 'sml/replacer-regexp-list '("^~/projects" ":proj:"))
+  (sml/setup))
 
 (use-package smart-tabs-mode
-             :ensure t
-             :config
-             (smart-tabs-insinuate 'c 'javascript)
-             (defadvice align (around smart-tabs activate)
-               (let ((indent-tabs-mode nil)) ad-do-it))
-             (defadvice align-regexp (around smart-tabs activate)
-               (let ((indent-tabs-mode nil)) ad-do-it)))
+  :ensure t
+  :config
+  (smart-tabs-insinuate 'c 'javascript)
+  (defadvice align (around smart-tabs activate)
+    (let ((indent-tabs-mode nil)) ad-do-it))
+  (defadvice align-regexp (around smart-tabs activate)
+    (let ((indent-tabs-mode nil)) ad-do-it)))
 
 (use-package anaconda-mode
-             :ensure t
-             :config
-             (progn (add-to-list 'company-backends 'company-anaconda)))
+  :ensure t
+  :config
+  (progn (add-to-list 'company-backends 'company-anaconda)))
 
 (use-package company-anaconda
-             :ensure t)
+  :ensure t)
 
 (use-package yasnippet
-             :ensure t
-             :config
-             (yas-global-mode 1))
+  :ensure t
+  :config
+  (yas-global-mode 1))
 
 (use-package flycheck
-             :ensure t
-             :init
-             (setq flycheck-jshintrc "~/.config/jshintrc"
-                   flycheck-flake8rc "~/.config/flake8"
-                   flycheck-check-syntax-automatically '(mode-enabled new-line save)
-                   flycheck-indication-mode 'left-fringe)
-             :config
-             (global-flycheck-mode))
+  :ensure t
+  :init
+  (setq flycheck-jshintrc "~/.config/jshintrc"
+        flycheck-flake8rc "~/.config/flake8"
+        flycheck-check-syntax-automatically '(mode-enabled new-line save)
+        flycheck-indication-mode 'left-fringe)
+  :config
+  (global-flycheck-mode))
 
 (use-package uniquify
-             :init
-             (setq uniquify-buffer-name-style 'forward))
+  :init
+  (setq uniquify-buffer-name-style 'forward))
 
 (use-package uncrustify-mode
-             :ensure t)
+  :ensure t)
 
 (use-package ace-jump-mode
-             :ensure t
-             :init
-             (setq ace-jump-mode-move-keys (append "iduhetonasyfpg.c,r;lxbkmjwqv'z" nil)))
+  :ensure t
+  :init
+  (setq ace-jump-mode-move-keys (append "iduhetonasyfpg.c,r;lxbkmjwqv'z" nil)))
 
 ;; Filetype modes
 (use-package python-mode
-             :ensure t
-             :init
-             (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-             (add-to-list 'interpreter-mode-alist '("python" . python-mode))
-             :config
-             (anaconda-mode)
-             (smart-tabs-mode-enable)
-             ; workaround, this needs to be python-indent-line instead of python-indent-line-1 which is the default advice
-             (smart-tabs-advice python-indent-line python-indent))
+  :ensure t
+  :init
+  (setq py-indent-offset (default-value 'tab-width)
+        py-indent-tabs-mode t)
+  (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+  (add-to-list 'interpreter-mode-alist '("python" . python-mode))
+  (add-hook 'python-mode-hook
+            (lambda () (run-hooks 'prog-mode-hook)))
+  :config
+  (anaconda-mode)
+  (smart-tabs-mode-enable)
+  ;; workaround, this needs to be python-indent-line instead of python-indent-line-1 which is the default advice
+  (smart-tabs-advice python-indent-line python-indent))
 
 (use-package json-mode
-             :ensure t
-             :init
-             (add-to-list 'auto-mode-alist '("\\.json$" . json-mode)))
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.json$" . json-mode)))
 
 (use-package js2-mode
-             :ensure t
-             :init
-             (setq js2-highlight-level 3
-                   js2-mode-show-parse-errors nil
-                   js2-mode-show-strict-warnings nil)
-             (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
+  :ensure t
+  :init
+  (setq js2-highlight-level 3
+        js2-mode-show-parse-errors nil
+        js2-mode-show-strict-warnings nil)
+  (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
 
 (use-package nginx-mode
-             :ensure t
-             :init
-             (add-to-list 'auto-mode-alist '("/etc/nginx/.*\\.conf$" . nginx-mode))
-             (add-to-list 'auto-mode-alist '(".*\\.nginx\\.conf$" . nginx-mode)))
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '("/etc/nginx/.*\\.conf$" . nginx-mode))
+  (add-to-list 'auto-mode-alist '(".*\\.nginx\\.conf$" . nginx-mode)))
 
 (use-package vcl-mode
-             :ensure t
-             :init
-             (add-to-list 'auto-mode-alist '(".*\\.vcl$" . vcl-mode)))
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '(".*\\.vcl$" . vcl-mode)))
 
 (use-package sql
-             :ensure t
-             :config
-             (sql-highlight-postgres-keywords))
+  :ensure t
+  :config
+  (sql-highlight-postgres-keywords))
 
 (use-package jade-mode
-             :ensure t
-             :init
-             (add-to-list 'auto-mode-alist '(".*\\.jade$" . jade-mode)))
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '(".*\\.jade$" . jade-mode)))
 
 (use-package stylus-mode
-             :ensure t)
+  :ensure t)
 
 (provide 'init)
 ;;; init.el ends here
